a58a6762618aa17dbff7af98fa9212c5b9ffdd4a
Matias Luraschi <luraschi8@gmail.com>
2016-03-21 18:18:29 -0300

    Agregada la lib de la db y creadas las clases para su uso.

---
+++ project/src/db/DBConnector.cpp
@@ -0,0 +1,18 @@
+/*
+ * DBConnector.cpp
+ *
+ *  Created on: Mar 21, 2016
+ *      Author: matias
+ */
+
+#include <DBConnector.h>
+
+DBConnector::DBConnector() {
+	// TODO Auto-generated constructor stub
+
+}
+
+DBConnector::~DBConnector() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ project/src/db/DBConnector.h
@@ -0,0 +1,17 @@
+/*
+ * DBConnector.h
+ *
+ *  Created on: Mar 21, 2016
+ *      Author: matias
+ */
+
+#ifndef SRC_DBCONNECTOR_H_
+#define SRC_DBCONNECTOR_H_
+
+class DBConnector {
+public:
+	DBConnector();
+	virtual ~DBConnector();
+};
+
+#endif /* SRC_DBCONNECTOR_H_ */
