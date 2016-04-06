9b95a2e69767e8af7b96161bf6ad1bceb076b681
Matias Luraschi <luraschi8@gmail.com>
2016-04-06 16:40:27 -0300

    Creadas mas preubas para el DBConnector y creados los servicios basicos del AutheticationService

--- project/test/applib/db/DBConnectorTest.cpp
+++ project/test/applib/db/DBConnectorTest.cpp
@@ -12,25 +12,41 @@ void DBConnectorTest::SetUp() {
 
 void DBConnectorTest::TearDown() {};
 
+int contarEntradas(DBConnector* connector) {
+	int contador = 0;
+	rocksdb::Iterator* it = connector->db->NewIterator(rocksdb::ReadOptions());
+	for (it->SeekToFirst(); it->Valid(); it->Next()) {
+		contador++;
+	}
+	delete it;
+	return contador;
+}
+
 TEST_F(DBConnectorTest, AddKeyToDB) {
 	DBConnector connector = DBConnector("/tmp/testdb/");
-	rocksdb::Iterator* it = connector.db->NewIterator(rocksdb::ReadOptions());
-	int contadorInicial = 0;
-	for (it->SeekToFirst(); it->Valid(); it->Next()) {
-		contadorInicial++;
-	}
-	ASSERT_TRUE(it->status().ok()); // Check for any errors found during the scan
-	delete it;
+	int contadorInicial = contarEntradas(&connector);
 	bool resultado = connector.putValueInKey("testKey", "testValue");
 	ASSERT_TRUE(resultado);
-	it = connector.db->NewIterator(rocksdb::ReadOptions());
-	int contadorFinal = 0;
-	for (it->SeekToFirst(); it->Valid(); it->Next()) {
-		contadorFinal++;
-	}
-	ASSERT_TRUE(it->status().ok()); // Check for any errors found during the scan
-	delete it;
+
+	int contadorFinal = contarEntradas(&connector);
 	ASSERT_TRUE(contadorFinal==contadorInicial+1);
+}
+
+TEST_F(DBConnectorTest, AddKeyToDBMultipleTimes) {
+	DBConnector connector = DBConnector("/tmp/testdb/");
+	bool resultado = connector.putValueInKey("twoTimesKey", "testValue");
+	ASSERT_TRUE(resultado);
+	int contadorMedio = contarEntradas(&connector);
+	resultado = connector.putValueInKey("twoTimesKey", "updatedValue");
+	int contadorFinal = contarEntradas(&connector);
+	ASSERT_TRUE(contadorFinal==contadorMedio);
+	ASSERT_TRUE(resultado);
+	std::string valor;
+	resultado = connector.getValueForKey("twoTimesKey", valor);
+	ASSERT_TRUE(resultado);
+	ASSERT_TRUE(valor == "updatedValue");
+	resultado = connector.deleteKey("twoTimesKey");
+	ASSERT_TRUE(resultado);
 }
 
 TEST_F(DBConnectorTest, RetriveValueFromDB) {
@@ -41,23 +57,30 @@ TEST_F(DBConnectorTest, RetriveValueFromDB) {
 	ASSERT_TRUE(key=="testValue");
 }
 
+TEST_F(DBConnectorTest, RetriveNonExistentValue) {
+	DBConnector connector = DBConnector("/tmp/testdb/");
+	std::string key;
+	bool resultado = connector.getValueForKey("nonExistent", key);
+	ASSERT_FALSE(resultado);
+}
+
 TEST_F(DBConnectorTest, DeleteValueFromDB) {
 	DBConnector connector = DBConnector("/tmp/testdb/");
-	rocksdb::Iterator* it = connector.db->NewIterator(rocksdb::ReadOptions());
-	int contadorInicial = 0;
-	for (it->SeekToFirst(); it->Valid(); it->Next()) {
-		contadorInicial++;
-	}
-	ASSERT_TRUE(it->status().ok()); // Check for any errors found during the scan
-	delete it;
+	int contadorInicial = contarEntradas(&connector);
 	bool resultado = connector.deleteKey("testKey");
 	ASSERT_TRUE(resultado);
-	it = connector.db->NewIterator(rocksdb::ReadOptions());
-	int contadorFinal = 0;
-	for (it->SeekToFirst(); it->Valid(); it->Next()) {
-		contadorFinal++;
-	}
-	ASSERT_TRUE(it->status().ok()); // Check for any errors found during the scan
-	delete it;
+
+	int contadorFinal = contarEntradas(&connector);
 	ASSERT_TRUE(contadorFinal==contadorInicial-1);
+}
+
+TEST_F(DBConnectorTest, DeleteNonExistentValueFromDB) {
+	DBConnector connector = DBConnector("/tmp/testdb/");
+	int contadorInicial = contarEntradas(&connector);
+	bool resultado = connector.deleteKey("nonExistent");
+	//OJO Borrar un valor que no existe devuelve true.
+	ASSERT_TRUE(resultado);
+
+	int contadorFinal = contarEntradas(&connector);
+	ASSERT_TRUE(contadorFinal==contadorInicial);
 }
