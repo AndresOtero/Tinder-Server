360dd3300bb4c3945b6ec9072c328bbd52eda2c1
Matias Luraschi <luraschi8@gmail.com>
2016-04-09 19:45:37 -0300

    Agregado elDAO para almacenar y obtener conversaciones

--- project/app/src/services/profile/ProfileServices.cpp
+++ project/app/src/services/profile/ProfileServices.cpp
@@ -13,17 +13,17 @@ ProfileServices::~ProfileServices() {}
 list<User *> ProfileServices::assembleUsersFromJson(Json::Value &root) {
 	list<User *> users;
 	for( Json::ValueIterator itr = root.begin() ; itr != root.end() ; itr++ ) {
-	unordered_map<string, set<string>> interests;
-	Json::Value user = itr->get("user","ERROR");
-	string userid = user.get("id", "ERROR").asString();
-	string name = user.get("name", "ERROR").asString();
-	string alias = user.get("alias", "ERROR").asString();
-	string email = user.get("email", "ERROR").asString();
-	string photo = user.get("photo_profile", "ERROR").asString();
-	Location location(user.get("location", "ERROR").get("latitude", "ERROR").asDouble(), 
-		user.get("location", "ERROR").get("longitude", "ERROR").asDouble());
-	User* newUser = new User(userid, name, alias, email, photo, interests, location);
-	users.push_back(newUser);
+		unordered_map<string, set<string>> interests;
+		Json::Value user = itr->get("user","ERROR");
+		string userid = user.get("id", "ERROR").asString();
+		string name = user.get("name", "ERROR").asString();
+		string alias = user.get("alias", "ERROR").asString();
+		string email = user.get("email", "ERROR").asString();
+		string photo = user.get("photo_profile", "ERROR").asString();
+		Location location(user.get("location", "ERROR").get("latitude", "ERROR").asDouble(), 
+			user.get("location", "ERROR").get("longitude", "ERROR").asDouble());
+		User* newUser = new User(userid, name, alias, email, photo, interests, location);
+		users.push_back(newUser);
 	}
 	return users;
 }
---
+++ project/test/applib/services/chat/ChatDAOTest.cpp
@@ -0,0 +1,65 @@
+#include "services/chat/ChatDAO.h"
+#include "ChatDAOTest.h"
+#include "db/DBConnector.h"
+#include "json/json/json.h"
+#include <string>
+
+ChatDAOTest::ChatDAOTest() {}
+
+ChatDAOTest::~ChatDAOTest() {};
+
+void ChatDAOTest::SetUp() {
+
+};
+
+void ChatDAOTest::TearDown() {};
+
+TEST_F(ChatDAOTest, saveMSGToDBAndRetrieve) {
+	DBConnector connector = DBConnector("/tmp/testChatDB/");
+	ChatDAO dao(&connector);
+	string mensaje = "hola gato";
+	bool resultado = dao.saveMsgFromTo(mensaje, "joni", "matias");
+	ASSERT_TRUE(resultado);
+
+	std::string retrieved;
+	bool resultado2 = dao.getMsgBetween("joni", "matias", retrieved);
+	ASSERT_TRUE(resultado2);
+
+	Json::Reader reader;
+	Json::Value value;
+	bool resultado3 = reader.parse(retrieved, value);
+	ASSERT_TRUE(resultado3);
+	ASSERT_TRUE(value.get("msg", "ERROR").asString() == mensaje);
+
+	//Limpio la DB
+	connector.deleteKey("joni-matias");
+}
+
+TEST_F(ChatDAOTest, retrieveMultipleMsgs) {
+	DBConnector connector = DBConnector("/tmp/testChatDB/");
+	ChatDAO dao(&connector);
+	string mensaje1 = "hola gato";
+	bool resultado = dao.saveMsgFromTo(mensaje1, "joni", "matias");
+	ASSERT_TRUE(resultado);
+	string mensaje2 = "chau gato";
+	resultado = dao.saveMsgFromTo(mensaje2, "joni", "matias");
+	ASSERT_TRUE(resultado);
+
+	std::string retrieved;
+	bool resultado2 = dao.getMsgBetween("joni", "matias", retrieved);
+	ASSERT_TRUE(resultado2);
+
+	Json::Reader reader;
+	Json::Value value;
+	bool resultado3 = reader.parse(retrieved, value);
+	ASSERT_TRUE(resultado3);
+
+	int contador = 0;
+	for( Json::ValueIterator itr = value.begin() ; itr != value.end() ; itr++ ) {
+		contador++;
+	}
+	ASSERT_TRUE(contador == 2);
+
+	//Limpio la DB
+	connector.deleteKey("joni-matias");
+}
