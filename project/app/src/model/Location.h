78a890fa2471de1bc88a5040bf399875c602818f
Matias Luraschi <luraschi8@gmail.com>
2016-04-15 00:05:53 -0300

    Agregados los comentarios para la docuementacion automatica

--- project/app/src/model/Location.h
+++ project/app/src/model/Location.h
@@ -9,14 +9,53 @@
 #define SRC_LOCATION_H_
 
 
+/**
+ * @class Location
+ * Represents the location of the user.
+ *
+ * @author MLuraschi
+ *
+ */
 class Location {
 public:
+
+	/**
+	* Constructor that sets the Location with initial values 0;
+    */
 	Location();
+
+	/**
+	 * Constructor that sets the Location.
+	 *
+	 * @param lat the latitude of the location to create;
+	 * @param lon the longitude of the location to create;
+	 *
+	 */
 	Location(double lat, double lon);
 	virtual ~Location();
+
+	/**
+	 * Returns de latitud of the location
+	 */
 	double getLatitude();
+
+	/**
+	 * Returns de longitud of the location.
+	 */
 	double getLongitude();
+
+	/**
+	 * Changes the latitude of the location
+	 *
+	 * @param latitude that will become the new value
+	 */
 	void setLatitude(double latitude);
+
+	/**
+	 * Changes the logitude of the location
+	 *
+	 * @param longitude that will become the new value
+	 */
 	void setLongitude(double longitude);
 
 private:
