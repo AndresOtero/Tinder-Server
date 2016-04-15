068d7f411145900eeb23b4321e699398197d96ef
Matias Luraschi <luraschi8@gmail.com>
2016-04-15 15:58:52 -0300

    Comentarios agregados para la documentacion al SharedConnector.

--- project/app/src/sharedConnector/SharedConnector.h
+++ project/app/src/sharedConnector/SharedConnector.h
@@ -12,16 +12,71 @@
 #include <curl/curl.h>
 #include "../json/json/json.h"
 
-
+/**
+ * @class SharedConnector
+ *
+ * Provides methods to interact with an API Rest.
+ */
 class SharedConnector {
 public:
+
+	/**
+	 * Default constructor.
+	 *
+	 * @param serverBaseURL base URL of the server where the API rest is.
+	 */
 	SharedConnector(std::string serverBaseURL);
 	virtual ~SharedConnector();
+
+	/**
+	 * Method that tries to reach the given base URL. It returns true or false whether the served is reachable or not.
+	 */
 	bool testConnection();
+
+	/**
+	 * Using the verb Get it hits the endpoint received and parses the respond as Json. It returns true or false if the
+	 * call was successful.
+	 *
+	 * @param endpoint endpoint to hit. It is going to be appended to the baseURL stored.
+	 * @param jsonData the parsed response is going to be stored here.
+	 */
 	bool getJsonFromURL(std::string endpoint, Json::Value& jsonData);
+
+	/**
+	 * Using the verb Put it send data to the endpoint received and stores the response. It returns true or false if
+	 * the call wass successful.
+	 *
+	 * @param endpoint to hit with the verb. It is appended to the base URL.
+	 * @param data to send to the endpoint.
+	 * @param response the response of the request is stored there.
+	 */
 	bool putDataToURL(std::string endpoint, std::string data, Json::Value& response);
+
+	/**
+	 * Using the verb Post it send data to the endpoint received and stores the response. It returns true or false if
+	 * the call wass successful.
+	 *
+	 * @param endpoint to hit with the verb. It is appended to the base URL.
+	 * @param data to send to the endpoint.
+	 * @param response the response of the request is stored there.
+	 */
 	bool postDataToURL(std::string endpoint, std::string data, Json::Value& response);
+
+	/**
+	 * Using the verb Post it send data to the endpoint received and stores the response as a Json. It returns true or false if
+	 * the call wass successful.
+	 *
+	 * @param endpoint to hit with the verb. It is appended to the base URL.
+	 * @param data to send to the endpoint.
+	 * @param response the response of the request is stored there.
+	 */
 	bool postDataToURL(std::string endpoint, std::string data, std::string &response);
+
+	/**
+	 * It send the verb DELETE to the given endpoint in the base url.
+	 *
+	 * @param endpoint to append to the base url.
+	 */
 	bool deleteToURL(std::string endpoint);
 	std::string serverBaseURL;
 
