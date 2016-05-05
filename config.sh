e2feb6b3e8d4dd789ba92d8f3bd346c80598c693
Jonathan <jonathan.tierno@gmail.com>
2016-05-05 17:11:18 -0300

    agregando configuración para coveral

--- config.sh
+++ config.sh
@@ -1,39 +1,164 @@
 #!/bin/bash
 # Instalación de dependencias y herramientas
-echo "Instalación de dependencias..."
-apt-get update -q
-apt-get install -y \
-autotools-dev \
-libicu-dev \
-build-essential \
-cmake \
-libbz2-dev \
-libcrypto++-dev \
-zlib1g-dev \
-libsnappy-dev \
-libcurl4-gnutls-dev \
-doxygen \
-lcov \
-gcovr
 
-if [ "$1" == "-oldboost" ]
+
+OLDBOOST=false
+COVERAGE=false
+COVERALLUPLOAD=false
+GCINCO=false
+TESTS=false
+DOXYDOC=false
+DEV=false
+DEPENDENCIES=false
+
+while test $# -gt 0; do
+        case "$1" in
+                -h|--help)
+                        echo "Configuration script"
+                        echo " "
+                        echo "config.sh [options]"
+                        echo " "
+                        echo "options:"
+                        echo "-h, --help    				imprimir esta ayuda 							"
+                        echo "-g++-5        				flag g++-5 para cmake 							"
+                        echo "-oldboost 				compatibilidad de boost para linux version 14.04"
+                        echo "-test					habilita los tests 								"
+                        echo "-coverage				habilita coverage y agrega -test 								"
+                        echo "-coverallupload				habilita la subida de coverall y -coverage 					"
+                        echo "-doxydoc				habilita documentación doxy 					"
+                        echo "-dependencies				instala dependencias"
+                        echo "--dev 					atajo para -test -doxydoc -coverall 		"
+                        exit 0
+                        ;;
+                -g++-5)
+						GCINCO=true
+                        shift
+                        ;;
+                -oldboost)
+						OLDBOOST=true
+                        shift
+                        ;;
+                -coverage)
+						COVERAGE=true
+                        shift
+                        ;;
+                -coverallupload)
+						COVERALLUPLOAD=true
+                        shift
+                        ;;
+                -doxydoc)
+						DOXYDOC=true
+                        shift
+                        ;;
+                -test)
+						TESTS=true	
+                        shift
+                        ;;
+                -dependencies)
+						DEPENDENCIES=true	
+                        shift
+                        ;;
+                --dev)
+						DEV=true
+                        shift
+                        ;;
+                *)
+                        break
+                        ;;
+        esac
+done
+
+if [ "$DEV" = true ] 
 then
- add-apt-repository ppa:afrank/boost -y
- apt-get update -q
- apt-get install libboost1.57-all libboost1.57-all-dev
-else
-	apt-get install -y libboost-all-dev
+	TESTS=true
+	DOXYDOC=true
+	COVERAGE=true
 fi
+
+if [ "$COVERALLUPLOAD" = true ] 
+then
+	COVERAGE=true
+fi
+
+if [ "$COVERAGE" = true ] 
+then
+	TESTS=true
+fi
+
+if [ "$DOXYDOC" = true ] 
+then
+	TESTS=true
+fi
+
+
+
+
+CMAKE_PARAMS=""
+
+if [ "$TESTS" = true ]
+	then
+	CMAKE_PARAMS="$CMAKE_PARAMS -TESTS=ON"
+fi
+if [ "$COVERAGE" = true ]
+	then
+	CMAKE_PARAMS="$CMAKE_PARAMS -COVERALL=ON"
+fi
+if [ "$COVERALLUPLOAD" = true ]
+	then
+	CMAKE_PARAMS="$CMAKE_PARAMS -COVERALLS_UPLOAD=ON"
+fi
+if [ "$DOXYDOC" = true ]
+	then
+	CMAKE_PARAMS="$CMAKE_PARAMS -BUILD_DOXY_DOC=ON"
+fi
+
+if [ "$GCINCO" = true ]
+	then
+	CMAKE_PARAMS="$CMAKE_PARAMS -DCMAKE_CXX_COMPILER='g++-5' -B."
+fi
+
+
 
 mkdir -p build
 cd build
 
-if [ "$1" == "-oldboost" -o "$1" == "-travis" ]
+
+echo $CMAKE_PARAMS
+
+if [ "$DEPENDENCIES" = true  ]
+then
+
+echo "Instalación de dependencias..."
+	apt-get update -q
+	apt-get install -y \
+	autotools-dev \
+	libicu-dev \
+	build-essential \
+	cmake \
+	libbz2-dev \
+	libcrypto++-dev \
+	zlib1g-dev \
+	libsnappy-dev \
+	libcurl4-gnutls-dev \
+	doxygen \
+	lcov \
+	gcovr
+	if [ "$OLDBOOST" = true ]
 	then
-	 cmake -DCMAKE_CXX_COMPILER='g++-5' -B. ../project && make
+	 add-apt-repository ppa:afrank/boost -y
+	 apt-get update -q
+	 apt-get install libboost1.57-all libboost1.57-all-dev
 	else
-	 cmake CMAKE_BUILD_TYPE=Debug ../project && make	
+		apt-get install -y libboost-all-dev
 	fi
+fi
+
+#if [ "$1" == "-oldboost" -o "$1" == "-travis" ]
+#	then
+#	 cmake  ../project && make
+#	else
+	 #cmake CMAKE_BUILD_TYPE=Debug ../project && make	
+	#fi
 
 
 
