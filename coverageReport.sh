5688363fe9d5b1f3d6afd0e5dacf74c2f813899e
Jonathan <jonathan.tierno@gmail.com>
2016-05-06 09:24:53 -0300

    #27 integrando con travis

--- coverageReport.sh
+++ coverageReport.sh
@@ -2,11 +2,12 @@ MYPWD=${PWD}
 GCOV_PREFIX=$MYPWD/build/coverage-data
 export GCOV_PREFIX="$MYPWD/build/coverage-data"
 export GCOV_PREFIX_STRIP=`echo $GCOV_PREFIX | tr -d -c '/\n' | awk '{ print length; }'`
+echo "Directorio donde almacenar $GCOV_PREFIX strips $GCOV_PREFIX_STRIP"
 cd build
-make
-echo  $GCOV_PREFIX
+echo "Creo los directorios necesarios"
 mkdir -p $GCOV_PREFIX/applib/CMakeFiles/testapp.dir
 mkdir -p $GCOV_PREFIX/CMakeFiles/applib.dir
+echo "Copiando gcno"
 rsync -acv --filter='+ */' --filter='+ *.cpp' --filter='+ *.h' --filter='+ *.gcno' --filter='- *' $MYPWD/build/test/applib/CMakeFiles/testapp.dir/ $GCOV_PREFIX/applib/CMakeFiles/testapp.dir
 rsync -acv --filter='+ */' --filter='+ *.cpp' --filter='+ *.h' --filter='+ *.gcno' --filter='- *' $MYPWD/build/app/CMakeFiles/applib.dir/src $GCOV_PREFIX/CMakeFiles/applib.dir
 
