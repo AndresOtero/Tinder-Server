a9600256accd06473a3baf5d8d4e17fb6173a777
Jonathan <jonathan.tierno@gmail.com>
2016-05-24 22:48:48 -0300

    configuracion de theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,7 +14,8 @@
 
 import sys
 import os
-import sphinx_bootstrap_theme
+#import sphinx_bootstrap_theme
+import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -115,16 +116,18 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+#html_theme = 'bootstrap'
+html_theme = "sphinx_rtd_theme"
+html_theme_path = ["_themes", ]
+#html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
-html_theme_options = {
+#html_theme_options = {
     # Navigation bar title. (Default: ``project`` value)
 
-    'navbar_title': "Match Application",
+ #   'navbar_title': "Match Application",
 
     # Tab name for entire site. (Default: "Site")
-    'navbar_site_name': "Contenido",
+  #  'navbar_site_name': "Contenido",
 
     # A list of tuples containing pages or urls to link to.
     # Valid tuples should be in the following forms:
@@ -139,17 +142,17 @@ html_theme_options = {
    # ],
 
     # Render the next and previous page links in navbar. (Default: true)
-    'navbar_sidebarrel': False,
+   # 'navbar_sidebarrel': False,
 
     # Render the current pages TOC in the navbar. (Default: true)
-    'navbar_pagenav': False,
+    #'navbar_pagenav': False,
 
     # Tab name for the current pages TOC. (Default: "Page")
-    'navbar_pagenav_name': "None",
+    #'navbar_pagenav_name': "None",
 
     # Global TOC depth for "site" navbar tab. (Default: 1)
     # Switching to -1 shows all levels.
-    'globaltoc_depth': -1,
+    #'globaltoc_depth': -1,
 
     # Include hidden TOCs in Site navbar?
     #
@@ -158,30 +161,30 @@ html_theme_options = {
     # will break.
     #
     # Values: "true" (default) or "false"
-    'globaltoc_includehidden': "true",
+    #'globaltoc_includehidden': "true",
 
     # HTML navbar class (Default: "navbar") to attach to <div> element.
     # For black navbar, do "navbar navbar-inverse"
-    'navbar_class': "navbar",
+    #'navbar_class': "navbar",
 
     # Fix navigation bar to top of page?
     # Values: "true" (default) or "false"
-    'navbar_fixed_top': "true",
+    #'navbar_fixed_top': "true",
 
     # Location of link to source.
     # Options are "nav" (default), "footer" or anything else to exclude.
-    'source_link_position': "footer",
+    #'source_link_position': "footer",
 
     # Bootswatch (http://bootswatch.com/) theme.
     #
     # Options are nothing (default) or the name of a valid theme
     # such as "amelia" or "cosmo".
-    'bootswatch_theme': "spacelab",
+    #'bootswatch_theme': "spacelab",
 
     # Choose Bootstrap version.
     # Values: "3" (default) or "2" (in quotes)
-    'bootstrap_version': "3",
-}
+    #'bootstrap_version': "3",
+#}
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
